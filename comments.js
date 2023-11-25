c2e791f7efb030d74f72afdf3494b3bc0d99b539
tanmay1501 <66867695+tanmay1501@users.noreply.github.com>
2023-11-25 03:34:36 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,63 +1,80 @@
-//create web server
-var express = require('express');
-var router = express.Router();
-var mongoose = require('mongoose');
-var Comment = mongoose.model('Comment');
+//Create web server
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
 
-//get all comments
-router.get('/comments', function(req, res, next) {
-  Comment.find(function(err, comments){
-    if(err){ return next(err); }
-    res.json(comments);
-  });
+//Create server
+const server = http.createServer((req, res) => {
+    //Get URL and parse it
+    const parsedUrl = url.parse(req.url, true);
+    //Get path
+    const path = parsedUrl.pathname;
+    //Trimming path
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    //Get query string as an object
+    const queryStringObject = parsedUrl.query;
+
+    //Get HTTP method
+    const method = req.method;
+
+    //Get headers as an object
+    const headers = req.headers;
+
+    //Get payload if any
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        //Choose handler
+        const chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+        //Construct data object to send to handler
+        const data = {
+            'trimmedPath': trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': helpers.parseJsonToObject(buffer)
+        };
+
+        //Route request to handler specified in the router
+        chosenHandler(data, (statusCode, payload) => {
+            //Use the status code called back by handler or default to 200
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+
+            //Use payload called back by handler or default to empty object
+            payload = typeof (payload) == 'object' ? payload : {};
+
+            //Convert payload to a string
+            const payloadString = JSON.stringify(payload);
+
+            //Return response
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            //Log the request path
+            console.log('Returning this response: ', statusCode, payloadString);
+        });
+    });
 });
 
-//post a comment
-router.post('/comments', function(req, res, next) {
-  var comment = new Comment(req.body);
-  comment.save(function(err, comment){
-    if(err){ return next(err); }
-    res.json(comment);
-  });
+//Start server
+server.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
 
-//preloading comment objects
-router.param('comment', function(req, res, next, id) {
-  var query = Comment.findById(id);
-  query.exec(function (err, comment){
-    if (err) { return next(err); }
-    if (!comment) { return next(new Error("can't find comment")); }
-    req.comment = comment;
-    return next();
-  });
-});
+//Define handlers
+const handlers = {};
 
-//return a comment
-router.get('/comments/:comment', function(req, res) {
-  res.json(req.comment);
-});
-
-//update a comment
-router.put('/comments/:comment/upvote', function(req, res, next) {
-  req.comment.upvote(function(err, comment){
-    if (err) { return next(err); }
-    res.json(comment);
-  });
-});
-
-//update a comment
-router.put('/comments/:comment/downvote', function(req, res, next) {
-  req.comment.downvote(function(err, comment){
-    if (err) { return next(err); }
-    res.json(comment);
-  });
-});
-
-//delete a comment
-router.delete('/comments/:comment', function(req, res) {
-  console.log("in delete");
-  req.comment.remove();
-  res.sendStatus(200);
-});
-
-module.exports = router;
+//Ping handler
+handlers.ping = (data  , callback) => {
+    callback(200);
+}
